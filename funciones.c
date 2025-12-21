1a6edd646e9bab65065880e54f5a06d6d0fe2acc
Steven Vaca <steven.vaca.galeas@udla.edu.ec>
2025-12-21 13:20:35 -0500

    Creado: Inicio

---
+++ funciones.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void menu(void){
+    printf("-------------MENU--------------\n");
+    printf("1. Listar vehiculos disponibles\n");
+    printf("2. Listar vehiculos disponibles\n");
+    printf("3. Listar vehiculos disponibles\n");
+    printf("4. Listar vehiculos disponibles\n");
+    printf("-------------------------------\n");
+    printf(">> ");
+}
+
+    
